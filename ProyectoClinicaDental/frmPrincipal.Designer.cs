9414c1db69029e305b841be93a59b6ff9fdbb7c5
Nestor_Leiva <nestorjoel2@gmail.com>
2023-10-04 12:08:36 -0600

    feat: initial commit

--- ProyectoClinicaDental/frmPrincipal.Designer.cs
+++ ProyectoClinicaDental/frmPrincipal.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            mnAcercaDe = new ToolStripMenuItem();
+            mnSalir = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem, mnAcercaDe, mnSalir });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(60, 20);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // mnAcercaDe
+            // 
+            mnAcercaDe.Name = "mnAcercaDe";
+            mnAcercaDe.Size = new Size(71, 20);
+            mnAcercaDe.Text = "Acerca de";
+            mnAcercaDe.Click += mnAcercaDe_Click;
+            // 
+            // mnSalir
+            // 
+            mnSalir.Name = "mnSalir";
+            mnSalir.Size = new Size(41, 20);
+            mnSalir.Text = "Salir";
+            mnSalir.Click += mnSalir_Click;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "frmPrincipal";
+            Text = "Clinica Dental Infantil Happy Teeth";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem mnAcercaDe;
+        private ToolStripMenuItem mnSalir;
     }
 }
--- ProyectoClinicaDental/frmPrincipal.cs
+++ ProyectoClinicaDental/frmPrincipal.cs
@@ -6,5 +6,17 @@ namespace ProyectoClinicaDental
         {
             InitializeComponent();
         }
+
+        private void mnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mnAcercaDe_Click(object sender, EventArgs e)
+        {
+            frmAcercaDe ventanaAcercade = new frmAcercaDe();
+            ventanaAcercade.MdiParent = this;
+            ventanaAcercade.Show();
+        }
     }
 }
