433d8f2bd6e64a224aa405571173b611e9ab5cab
Nestor_Leiva <nestorjoel2@gmail.com>
2023-10-04 12:42:07 -0600

    feat: Front-End ventana Acerca De

--- ProyectoClinicaDental/frmAcercaDe.Designer.cs
+++ ProyectoClinicaDental/frmAcercaDe.Designer.cs
@@ -28,12 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAcercaDe";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAcercaDe));
+            pbLogo = new PictureBox();
+            lblTitulo = new Label();
+            lblTitulo1 = new Label();
+            lblEstudiante1 = new Label();
+            lblEstudiante = new Label();
+            lblProfesor = new Label();
+            lblProfesor1 = new Label();
+            lbl_Institucion = new Label();
+            lbl_Institucion1 = new Label();
+            lblCurso1 = new Label();
+            lblCurso = new Label();
+            lblAnio1 = new Label();
+            lblAnio = new Label();
+            btnCerrar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pbLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // pbLogo
+            // 
+            pbLogo.Image = (Image)resources.GetObject("pbLogo.Image");
+            pbLogo.Location = new Point(31, 78);
+            pbLogo.Name = "pbLogo";
+            pbLogo.Size = new Size(290, 359);
+            pbLogo.TabIndex = 0;
+            pbLogo.TabStop = false;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTitulo.Location = new Point(401, 21);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(153, 23);
+            lblTitulo.TabIndex = 1;
+            lblTitulo.Text = "Proyecto #1";
+            lblTitulo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblTitulo1
+            // 
+            lblTitulo1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTitulo1.Location = new Point(363, 60);
+            lblTitulo1.Name = "lblTitulo1";
+            lblTitulo1.Size = new Size(227, 74);
+            lblTitulo1.TabIndex = 2;
+            lblTitulo1.Text = "Clinica Dental Happy Teeth";
+            lblTitulo1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblEstudiante1
+            // 
+            lblEstudiante1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblEstudiante1.Location = new Point(456, 261);
+            lblEstudiante1.Name = "lblEstudiante1";
+            lblEstudiante1.Size = new Size(206, 32);
+            lblEstudiante1.TabIndex = 3;
+            lblEstudiante1.Text = "Nestor Leiva Villalobos";
+            lblEstudiante1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblEstudiante
+            // 
+            lblEstudiante.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblEstudiante.Location = new Point(363, 229);
+            lblEstudiante.Name = "lblEstudiante";
+            lblEstudiante.Size = new Size(108, 32);
+            lblEstudiante.TabIndex = 4;
+            lblEstudiante.Text = "Estudiante:";
+            lblEstudiante.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblProfesor
+            // 
+            lblProfesor.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblProfesor.Location = new Point(363, 293);
+            lblProfesor.Name = "lblProfesor";
+            lblProfesor.Size = new Size(88, 32);
+            lblProfesor.TabIndex = 5;
+            lblProfesor.Text = "Profesor:";
+            lblProfesor.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblProfesor1
+            // 
+            lblProfesor1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblProfesor1.Location = new Point(456, 325);
+            lblProfesor1.Name = "lblProfesor1";
+            lblProfesor1.Size = new Size(206, 40);
+            lblProfesor1.TabIndex = 6;
+            lblProfesor1.Text = "Roger Rodrigez Lopez";
+            lblProfesor1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lbl_Institucion
+            // 
+            lbl_Institucion.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_Institucion.Location = new Point(363, 154);
+            lbl_Institucion.Name = "lbl_Institucion";
+            lbl_Institucion.Size = new Size(108, 32);
+            lbl_Institucion.TabIndex = 7;
+            lbl_Institucion.Text = "Institucion:";
+            lbl_Institucion.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lbl_Institucion1
+            // 
+            lbl_Institucion1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_Institucion1.Location = new Point(456, 186);
+            lbl_Institucion1.Name = "lbl_Institucion1";
+            lbl_Institucion1.Size = new Size(289, 43);
+            lbl_Institucion1.TabIndex = 8;
+            lbl_Institucion1.Text = "Colegio Universitario de Cartago";
+            lbl_Institucion1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblCurso1
+            // 
+            lblCurso1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblCurso1.Location = new Point(441, 402);
+            lblCurso1.Name = "lblCurso1";
+            lblCurso1.Size = new Size(236, 32);
+            lblCurso1.TabIndex = 9;
+            lblCurso1.Text = "TI-131 Programación II G3";
+            lblCurso1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblCurso
+            // 
+            lblCurso.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblCurso.Location = new Point(363, 361);
+            lblCurso.Name = "lblCurso";
+            lblCurso.Size = new Size(72, 41);
+            lblCurso.TabIndex = 10;
+            lblCurso.Text = "Curso:";
+            lblCurso.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblAnio1
+            // 
+            lblAnio1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblAnio1.Location = new Point(441, 467);
+            lblAnio1.Name = "lblAnio1";
+            lblAnio1.Size = new Size(63, 23);
+            lblAnio1.TabIndex = 12;
+            lblAnio1.Text = "2023";
+            lblAnio1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblAnio
+            // 
+            lblAnio.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblAnio.Location = new Point(363, 439);
+            lblAnio.Name = "lblAnio";
+            lblAnio.Size = new Size(49, 30);
+            lblAnio.TabIndex = 13;
+            lblAnio.Text = "Ano";
+            lblAnio.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Location = new Point(670, 519);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(75, 23);
+            btnCerrar.TabIndex = 14;
+            btnCerrar.Text = "Cerrar";
+            btnCerrar.UseVisualStyleBackColor = true;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // frmAcercaDe
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(782, 561);
+            Controls.Add(btnCerrar);
+            Controls.Add(lblAnio);
+            Controls.Add(lblAnio1);
+            Controls.Add(lblCurso);
+            Controls.Add(lblCurso1);
+            Controls.Add(lbl_Institucion1);
+            Controls.Add(lbl_Institucion);
+            Controls.Add(lblProfesor1);
+            Controls.Add(lblProfesor);
+            Controls.Add(lblEstudiante);
+            Controls.Add(lblEstudiante1);
+            Controls.Add(lblTitulo1);
+            Controls.Add(lblTitulo);
+            Controls.Add(pbLogo);
+            Name = "frmAcercaDe";
+            Text = "frmAcercaDe";
+            ((System.ComponentModel.ISupportInitialize)pbLogo).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pbLogo;
+        private Label lblTitulo;
+        private Label lblTitulo1;
+        private Label lblEstudiante1;
+        private Label lblEstudiante;
+        private Label lblProfesor;
+        private Label lblProfesor1;
+        private Label lbl_Institucion;
+        private Label lbl_Institucion1;
+        private Label lblCurso1;
+        private Label lblCurso;
+        private Label lblAnio1;
+        private Label lblAnio;
+        private Button btnCerrar;
     }
 }
