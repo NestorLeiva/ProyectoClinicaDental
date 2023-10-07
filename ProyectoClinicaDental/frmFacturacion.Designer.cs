f98d9406d40cf258defdf74d25d6536d2cdcb0d4
Nestor_Leiva <nestorjoel2@gmail.com>
2023-10-07 15:15:43 -0600

    refactor: Front-End

--- ProyectoClinicaDental/frmFacturacion.Designer.cs
+++ ProyectoClinicaDental/frmFacturacion.Designer.cs
@@ -28,18 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dataGridView1 = new DataGridView();
+            btnAceptar = new Button();
+            btnCancelar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // frmVentas
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(44, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(135, 29);
+            label1.TabIndex = 0;
+            label1.Text = "Cliente";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(44, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(135, 37);
+            label2.TabIndex = 1;
+            label2.Text = "Identificacion";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(574, 43);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 23);
+            label3.TabIndex = 2;
+            label3.Text = "N. Factura";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            label4.BackColor = SystemColors.ButtonShadow;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(574, 83);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 23);
+            label4.TabIndex = 3;
+            label4.Text = "0";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(44, 454);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 23);
+            label5.TabIndex = 4;
+            label5.Text = "SubTotal";
+            label5.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            label6.BackColor = SystemColors.ActiveBorder;
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(150, 454);
+            label6.Name = "label6";
+            label6.Size = new Size(100, 23);
+            label6.TabIndex = 5;
+            label6.Text = "0.0";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            label7.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(468, 454);
+            label7.Name = "label7";
+            label7.Size = new Size(100, 23);
+            label7.TabIndex = 6;
+            label7.Text = "Total";
+            label7.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label8
+            // 
+            label8.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(44, 496);
+            label8.Name = "label8";
+            label8.Size = new Size(100, 23);
+            label8.TabIndex = 7;
+            label8.Text = "IVA 13%";
+            label8.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label9
+            // 
+            label9.BackColor = SystemColors.ActiveBorder;
+            label9.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(574, 454);
+            label9.Name = "label9";
+            label9.Size = new Size(100, 23);
+            label9.TabIndex = 8;
+            label9.Text = "0.0";
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            label10.BackColor = SystemColors.ActiveBorder;
+            label10.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(150, 496);
+            label10.Name = "label10";
+            label10.Size = new Size(100, 23);
+            label10.TabIndex = 9;
+            label10.Text = "0.0";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(185, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(344, 33);
+            textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(185, 79);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(344, 33);
+            textBox2.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(44, 130);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(630, 306);
+            dataGridView1.TabIndex = 16;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAceptar.Location = new Point(468, 496);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(100, 31);
+            btnAceptar.TabIndex = 17;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCancelar.Location = new Point(574, 496);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(100, 31);
+            btnCancelar.TabIndex = 18;
+            btnCancelar.Text = "Cancerlar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmFacturacion
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "frmVentas";
+            ClientSize = new Size(712, 551);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnAceptar);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmFacturacion";
             Text = "Ventas";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DataGridView dataGridView1;
+        private Button btnAceptar;
+        private Button btnCancelar;
     }
 }
--- ProyectoClinicaDental/frmFuncionarios.Designer.cs
+++ ProyectoClinicaDental/frmFuncionarios.Designer.cs
@@ -28,18 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(312, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Ingrese los datos para Nuevos Funcionarios";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // frmFuncionarios
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Name = "frmFuncionarios";
             Text = "Nuevos Funcionarios";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
