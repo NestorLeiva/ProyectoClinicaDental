12dcc0d61c26ce3e1a343fbb075b88b166e98882
Nestor_Leiva <nestorjoel2@gmail.com>
2023-10-07 23:30:13 -0600

    feat: creacion de Clases

---
+++ BLL/DatosXML.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class DatosXML
+    {
+
+        /*
+         * esta clase manejara los XML en Lectura, Escritura / Serializacion Json*
+         * 
+         * simplificar datos con DataTables, Listas
+         * 
+         */
+        
+
+    }
+}
---
+++ BLL/Negocios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class Negocios
+    {
+        /*
+         * Clase para la Validacion de los Datos y 
+         * otras funcionalidades         
+         */
+    }
+}
